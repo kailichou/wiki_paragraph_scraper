0f26398725676d9553acefb5cf66f884c17da0bc
kailichou <35256506+kailichou@users.noreply.github.com>
2020-12-06 22:49:02 +0800

    add pages func

--- wikispider.py
+++ wikispider.py
@@ -4,15 +4,11 @@ import requests
 from bs4 import BeautifulSoup as soup
 
 class wikispider:
-    url='https://en.wikipedia.org/wiki/Data_analysis'
+    urls=['https://en.wikipedia.org/wiki/Artificial_intelligence#cite_note-AI_in_2000s-16','https://en.wikipedia.org/wiki/Big_data','https://en.wikipedia.org/wiki/Data_analysis']
 
-    def download(self):
-        if isinstance(self,str):
-            print("Start downloading page")
-            return requests.get(self).text
-        
-        else:
-            pass
+    def download(self,url):
+        print("Start downloading page")
+        return requests.get(self).text
 
     def parse_paragraph(self,content,parser='html.parser'):
         html=soup(content,parser)
@@ -20,18 +16,21 @@ class wikispider:
         return [result.text for result in results]
 
     def savefile(self,data):
-        with open('wikiscrape.txt','w') as file:
+        with open('wikiscrape.txt','a+') as file:
             for d in data:
                 file.write(d)
 
-    def main(self):
-        content=wikispider.download(self.url)
+    def main(self,url):
+        content=wikispider.download(self,url)
         para=self.parse_paragraph(content)
         self.savefile(para)
         print("FILE SAVED")
 
 
-
 if __name__=="__main__":
     spider=wikispider()
-    spider.main()
+    if isinstance(spider.urls, list):
+        for url in spider.urls:
+            spider.main(url)
+    elif isinstance(spider.urls, str):
+        spider.main(url)
